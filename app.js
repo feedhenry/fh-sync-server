681cd931d076baf6405ed779ac07e0bf01987ed4
Aiden Keating <akeating@redhat.com>
2017-09-05 14:18:09 +0100

    Use mcp mounted secrets by default

--- app.js
+++ app.js
@@ -6,7 +6,17 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const Keycloak = require('keycloak-connect');
 
-const authEnabled = !!process.env.SYNC_ENABLE_AUTH;
+const keycloakConfigPath = process.env.SYNC_KEYCLOAK_CONFIG || '/etc/secrets/keycloak-bearer-token/installation';
+
+var keycloakConfig;
+
+try {
+  keycloakConfig = require(keycloakConfigPath);
+} catch(e) {
+  console.log('Keycloak config not found at ' + keycloakConfigPath + ', auth will not be enabled');
+}
+
+// const authEnabled = !!process.env.SYNC_ENABLE_AUTH;
 
 /**
  * Get configuration for MongoDB to be used with sync.
@@ -64,7 +74,7 @@ sync.connect(mongodbConnectionString, mongoOptions, redisUrl, function startAppl
    * so this should not be set to false if auth should be disabled, instead it
    * should just not be defined.
    */
-  if(authEnabled) {
+  if(keycloakConfig) {
     console.log('Initialising Keycloak authentication');
 
     // Initialise the memory store for Keycloak.
@@ -83,6 +93,33 @@ sync.connect(mongodbConnectionString, mongoOptions, redisUrl, function startAppl
     app.use(keycloak.middleware());
   }
 
+
+  /**
+   * Filter each action, check whether the user sending the request has the
+   * ability to perform the action. There are only two roles in relation to
+   * authorization, sync_user_read and sync_user_write, these should map to the
+   * sync actions: create, update and delete.
+   *
+   * This assumes that the user is authenticated. Previous middleware should
+   * have performed this action.
+   *
+   * The read-only users should not have anything in their payloads. If they do
+   * then delete them.
+   *
+   * The users with write permissions can have anything they want in their
+   * payloads. It would also be possible to narrow down permissions to each
+   * individual action instead.
+   */
+  function filterPendingForPermission(requiredRole) {
+    return function(req, res, next) {
+      if(keycloakConfig && req.body.pending && req.body.pending.length > 0 && !req.kauth.grant.access_token.hasRole(requiredRole)) {
+        req.body.pending = [];
+        return next()
+      }
+      return next();
+    }
+  }
+
   /**
    * Small wrapper around Keycloak protect() middleware. Will only perform
    * authorization if the SYNC_ENABLE_AUTH environment variable is defined.
@@ -92,7 +129,7 @@ sync.connect(mongodbConnectionString, mongoOptions, redisUrl, function startAppl
    */
   function protectEndpoint(requiredRole) {
     return function(req, res, next) {
-      if(authEnabled) {
+      if(keycloakConfig) {
         return keycloak.protect(requiredRole)(req, res, next);
       }
       return next();
@@ -106,7 +143,7 @@ sync.connect(mongodbConnectionString, mongoOptions, redisUrl, function startAppl
   /**
    * Expose sync to clients.
    */
-  app.post('/sync/:datasetId', protectEndpoint('realm:sync_user'), function (req, res) {
+  app.post('/sync/:datasetId', protectEndpoint('realm:sync_read'), filterPendingForPermission('realm:sync_write'), function (req, res) {
     var datasetId = req.params.datasetId;
     var params = req.body;
 
